9dc4b38eff9b3b1026c0597204f39dff9f4d9b1c
sgaikar1 <santoshgaikar1@gmail.com>
2017-05-27 20:23:54 +0530

    Initial commit

---
+++ app/src/main/java/com/sgaikar1/kotlinpractice/JavaMainActivity.java
@@ -0,0 +1,15 @@
+package com.sgaikar1.kotlinpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by santosh on 27/5/17.
+ */
+
+public class JavaMainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/sgaikar1/kotlinpractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sgaikar1.kotlinpractice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
